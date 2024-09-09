7ee48dacb50d5a806159d6ac46dd38179b4e96d7
Ewa_Rayska <rayskaewa@gmail.com>
2024-09-09 22:40:32 +0200

    Add constroctor with parameters to Book class and create Book instances

--- LibraryApp/Book.cs
+++ LibraryApp/Book.cs
@@ -6,5 +6,10 @@
         public string? Title { get; set; }
         public string? Author { get; set; }
         public Book() { }
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
     }
 }
--- LibraryApp/Program.cs
+++ LibraryApp/Program.cs
@@ -1,3 +1,4 @@
+using LibraryApp;
 using System.Diagnostics.Eventing.Reader;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,20 +14,19 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-var titles = new[]
-{
-    "W pustyni i w puszczy",
-    "Krzy¿acy",
-    "Kubuœ Puchatek",
-    "Lalka",
-    "D¿uma",
-    "Tango",
-    "Akademia Pana Kleksa",
-    "Harry Potter",
-    "Hobbit",
-    "Pan Tadeusz",
-    "Balladyna"
-};
+Dictionary<string, string> titles = new Dictionary<string, string>();
+
+Book b1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz");
+Book b2 = new Book("Krzy¿acy", "Henryk Sienkiewicz");
+Book b3 = new Book("Kubuœ Puchatek", "A.A. Milne");
+Book b4 = new Book("Lalka", "Boleslaw Prus");
+Book b5 = new Book("D¿uma", "Albert Camus");
+Book b6 = new Book("Tango", "Slawomir Mrozek");
+Book b7 = new Book("Akademia Pana Kleksa", "Jan Brzechwa");
+Book b8 = new Book("Harry Potter", "J.K. Rowling");
+Book b9 = new Book("Hobbit", "J.R.R. Tolkien");
+Book b10 = new Book("Pan Tadeusz", "Adam Mickiewicz");
+Book b11 = new Book("Balladyna", "Juliusz Slowacki");
 
 app.MapGet("/weatherforecast", () =>
 {
@@ -44,29 +44,30 @@ app.MapGet("/weatherforecast", () =>
 app.MapGet("/books", () =>
 {
     app.Logger.LogInformation("I'm running on development");
-    var books = Enumerable.Range(1, titles.Length).Select(index =>
+    var books = Enumerable.Range(1, titles.Count).Select(index =>
     new Book
     (
             index,
-            titles[(index - 1)]
+            titles.Keys.ElementAt(index),
+            titles.Values.ElementAt(index)
         ))
     .ToArray();
     return books;
 });
 
 app.MapGet("/books/{title}", (string title) =>
-    titles.FirstOrDefault(t => t == title)
+    titles.Keys.FirstOrDefault(t => t == title)
 );
+
+/*app.MapGet("/books/{author}", (string author) =>
+    titles.Values.FirstOrDefault(a => a == author)
+);*/
 
 app.Run();
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-
-public record Book(int Id, string? Title)
-{
 }
 
 public partial class Program { }
