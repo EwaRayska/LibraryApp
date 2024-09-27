dd72d520d982601c86aa12decb0d285f949daf2b
Ewa_Rayska <rayskaewa@gmail.com>
2024-09-28 00:11:52 +0200

    Edit API endpoints by injection IBookRepository

--- LibraryApp/Program.cs
+++ LibraryApp/Program.cs
@@ -11,72 +11,39 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-var summaries = new[]
+app.MapGet("/books", (IBookRepository repository) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-var books = new List<Book>
-{
-    new (1, "W pustyni i w puszczy", "Henryk Sienkiewicz"),
-    new (2, "Krzy¿acy", "Henryk Sienkiewicz"),
-    new (3, "Kubuœ Puchatek", "A.A. Milne"),
-    new (4, "Lalka", "Boleslaw Prus"),
-    new (5, "D¿uma", "Albert Camus"),
-    new (6, "Tango", "Slawomir Mrozek"),
-    new (7, "Akademia Pana Kleksa", "Jan Brzechwa"),
-    new (8, "Harry Potter", "J.K. Rowling"),
-    new (9, "Hobbit", "J.R.R. Tolkien"),
-    new (10, "Pan Tadeusz", "Adam Mickiewicz"),
-    new (11, "Balladyna", "Juliusz Slowacki")
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return Results.Ok(repository.GetAllBooks());
 });
 
-app.MapGet("/books", () => books);
-
-app.MapGet("/books/{id}", (int id) =>
+app.MapGet("/books/{id}", (int id, IBookRepository repository) =>
 {
-    var book = books.FirstOrDefault(b => b.Id == id);
-    if (book == null)
-    {
-        return Results.NotFound();
-    }
-    return Results.Ok(book);
+    var book = repository.GetBookById(id);
+    return book != null ? Results.Ok(book) : Results.NotFound();
 });
 
-/*app.MapGet("/books/{author}", (string author) =>
-    titles.Values.FirstOrDefault(a => a == author)
-);*/
-
-app.MapPost("/books", (Book newBook) =>
-{ 
-    newBook.Id = books.Max(b => b.Id) + 1;
-    books.Add(newBook);
-
-    return Results.Created($"/books/{newBook.Id}", newBook);
+app.MapPost("/books", (Book book, IBookRepository repository) =>
+{
+    repository.AddBook(book);
+    return Results.Created($"/books/{book.Id}", book);
  });
 
-app.MapDelete("/books/{id}", (int id) =>
+app.MapPut("/books/{id}", (int id, Book book, IBookRepository repository) =>
 {
-    var book = books.FirstOrDefault(b => b.Id == id);
-    if (book == null)
-    {
-        return Results.NotFound();
-    }
+    var existingBook = repository.GetBookById(id);
+    if(existingBook == null) return Results.NotFound();
 
-    books.Remove(book);
+    book.Id = id;
+    repository.UpdateBook(book);
+    return Results.NoContent();
+});
+
+app.MapDelete("/books/{id}", (int id, IBookRepository repository) =>
+{
+    var book = repository.GetBookById(id);
+    if (book != null) return Results.NotFound();
+
+    repository.DeleteBook(id);
     return Results.NoContent();
 });
 
